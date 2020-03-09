4a8e7fc954ae6854fb83b36db2a0b53bb5f23d4a
Nikhil Lohia <nikoo28@gmail.com>
2020-03-08 17:47:45 -0700

    Update project files Add gitignore files

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func changeButton(_ sender: Any) {
+        
+        if mainImageView.image == UIImage(named: "back") {
+            mainImageView.image = UIImage(named: "front")
+        }
+        else {
+        mainImageView.image = UIImage(named: "back")
+        }
+    }
 }
 
